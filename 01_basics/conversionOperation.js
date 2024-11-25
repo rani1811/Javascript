669035367da48efce4eb053ecab53f73eaf8a6c2
rani1811 <raniwingale@gmail.com>
2024-11-25 06:33:36 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,34 @@
+let score = 44;
+
+// console.log(typeof score);
+// console.log(typeof (score));//alternative way
+
+//let value = "44" ;  //convert to 33 
+//let value = "44ab" ;// not convert give a value NAN
+//let value = null ; //convert to 0
+
+//let value = undefined
+
+//let value =true //convert to 1 and false to 0
+
+
+
+// console.log(typeof value)
+// let valueInNumber = Number(value)
+// console.log( valueInNumber) ;
+
+
+
+//let islogginIn =1 //convert to true and 0 to false 
+//let islogginIn ="" //false 
+// let islogginIn ="rani" // true
+// let booleanIsLoggIn =Boolean(islogginIn)
+// console.log(booleanIsLoggIn)
+
+
+let someNumber =33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+
+console.log(typeof stringNumber)
