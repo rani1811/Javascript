ccde86a0b7205aea5eff912730548788452fb287
rani1811 <raniwingale@gmail.com>
2024-11-25 04:45:56 +0000

    variable declaration

---
+++ 01_basics/variable.js
@@ -0,0 +1,25 @@
+const accountId = 123456
+let accountEmailId ="google@google.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+
+console.log(accountId);
+
+
+/* 
+
+prefer not to use var 
+because of issue in block scope and functional scope
+
+
+*/
+
+//accountId =2;
+accountEmailId ="github@gmail.com"
+accountPassword="23444"
+accountCity ="shirdi"
+
+console.table([accountEmailId,accountId,accountCity])
+
+let accountstate;
+console.log(accountstate)
