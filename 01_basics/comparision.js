a0312d03a69d6920c28c7a90bd7d720ba886e48c
rani1811 <raniwingale@gmail.com>
2024-12-02 04:19:19 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,14 @@
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+
+// === strict check - check value and datatype
+
+// console.log ("2" === 2)
--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -3,7 +3,7 @@ let score = 44;
 // console.log(typeof score);
 // console.log(typeof (score));//alternative way
 
-//let value = "44" ;  //convert to 33 
+//let value = "44" ;  //convert to 44
 //let value = "44ab" ;// not convert give a value NAN
 //let value = null ; //convert to 0
 
@@ -26,9 +26,25 @@ let score = 44;
 // console.log(booleanIsLoggIn)
 
 
-let someNumber =33 
+// let someNumber =33 
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+//***********************operations************************
+
+
+// let value = 3 
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 =" world"
+// let str3 = str1 + str2
+// console.log(str3)
+
+
