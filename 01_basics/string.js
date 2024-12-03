62fd61f2f6651a4324bd183e0961127eeeb150be
rani1811 <raniwingale@gmail.com>
2024-12-03 04:41:10 +0000

    string methods

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,4 +1,37 @@
-const name ="rani"
+//const name ="rani"
+const name ="rani-ingale"
 const repoCount = 2 
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) //string interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) //string interpolation
+
+const gameName = new String("rani");
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(3))
+
+// console.log(gameName.indexOf('n'))
+
+
+// const newString = gameName.substring(0,4)
+
+// console.log(newString)
+
+// const anotherString = gameName.substring(-3,4)
+
+
+// console.log(anotherString)
+
+
+
+
+const url ="https://ee.co.uk.com/effective%20flesta"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('wr9x'))
+
+console.log(url.split('/'))
