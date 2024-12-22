acb509ad40b88db59f429fc0d0e074a023eb277e
rani1811 <raniwingale@gmail.com>
2024-12-22 14:51:33 +0000

    Structuring & destructring

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -1,5 +1,48 @@
 //singleton
 
 const regular_user ={
-    
+    email:"user@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"rani",
+            lastname:"ingale"
+        }
+    }
 }
+
+//console.log(regular_user.fullname.userfullname.firstname)
+
+const obj1={1:"a" ,2:"b"}
+const obj2={3:"c",4:"d"}
+
+//const obj3={obj1,obj2}
+
+//const obj3 =Object.assign({},obj1,obj2)
+
+const obj3 ={...obj1, ...obj2}
+// console.log(obj3)
+
+// console.log(Object.keys(regular_user))
+// console.log(Object.values(regular_user))
+// console.log(Object.entries(regular_user))
+
+// console.log(regular_user.hasOwnProperty('islogged'))
+
+
+//destructring
+const course ={
+    coursename:"java",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+//course.courseInstructor
+
+
+const {courseInstructor:mentor} =course
+
+console.log(mentor)
+
+
+
+
