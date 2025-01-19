ce591e439803f367e6f802abbc217bed6cf5ea11
rani1811 <raniwingale@gmail.com>
2025-01-19 17:01:16 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+//switch 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = 2;
+//you can used string also const="march"
+ 
+
+switch (month) {
+    case 1: //case "jan":
+        console.log("January")
+        break;
+    case 2://case "feb":
+        console.log("feb")
+      break;
+    case 3:
+        console.log("march")
+        break;
+
+    default:
+        console.log("default")
+        break;
+}
